bfe79c2761d2f37fe03edd4a4794ebf47ec14478
Pedro Vinícius de Oliveira Pequeno <pedro.pequeno@fvncional.com.br>
2018-08-31 09:17:50 -0400

    Pedro - Primeiro COmmit

---
+++ src/main/java/com/br/nescaupower/KeepSoftAPI/KeepSoftApiApplication.java
@@ -0,0 +1,12 @@
+package com.br.nescaupower.KeepSoftAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepSoftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepSoftApiApplication.class, args);
+	}
+}
